d4ccfee81430d44035e4b3cde7d634f5041bfca2
Omar Shorbaji <github@shorbaji.com>
2024-04-11 13:32:51 +0400

    bugfix issue 1

--- tests/entity_test.py
+++ tests/entity_test.py
@@ -14,6 +14,12 @@ valid["ISO11783_TaskData"] = """<ISO11783_TaskData
     </ISO11783_TaskData>"""
 
 valid["AFE"] = """ <AFE A="foo.txt" B="True" C="GLN" D="txt" ></AFE>"""
+valid["PNT"] = '<PNT A="10" C="49.3682793876954" D="9.55990880103963" />'
+valid["LSG"] = """
+<LSG A="1">
+                <PNT A="10" C="49.3682793876954" D="9.55990880103963" />
+            </LSG>
+"""
 
 def test_taskdata_blank_fail():
     ''' Test that missing required fields in taskdata raises an exception '''
